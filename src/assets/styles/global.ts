d5d612da4b4a536fa81b574e2755dcb53491cb92
martins20 <pdesignerpq@gmail.com>
2020-08-04 10:52:36 -0300

    Finish global style

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
+
+
 export default createGlobalStyle`
+
     :root {
     --color-background: #F0F0F7;
     --color-primary-lighter: #9871F5;
@@ -33,4 +36,30 @@ export default createGlobalStyle`
   html, body, #root {
     height: 100vh;
   } 
+
+  body {
+    background: var(--color-background)
+  }
+
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  body, input, button, textarea {
+
+    font: 500 1.6rem Poppins
+  }
+
+  .container {
+    width: 90vw;
+    max-width: 700px;
+  }
+
+  @media (min-width: 700px) {
+    :root {
+      font-size: 62.5%
+    }
+  }
 `;
